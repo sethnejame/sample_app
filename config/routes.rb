e6688d2143f2cfa69f7c2659a5ee80bc2c4cd225
Seth NeJame <sethnejame@gmail.com>
2019-07-24 08:37:42 +0200

    Adding tests for signup path and linking on home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  get 'users/new'
   root "static_pages#home"
   # get 'static_pages/help'
   get '/help', to: 'static_pages#help'
@@ -9,5 +10,9 @@ Rails.application.routes.draw do
 
   # get 'static_pages/contact'
   get '/contact', to: 'static_pages#contact'
+
+  # create a new path (example.com/signup) and link it to 
+  # the controller (users) and the view (users/new)
+  get '/signup', to: 'users#new'
   
 end
