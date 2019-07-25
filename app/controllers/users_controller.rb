d338cf4bad7cd1d9594211320fde4c55a773ef1b
Seth NeJame <sethnejame@gmail.com>
2019-07-25 12:44:41 +0200

    Adding flash messages, user signup redirects to profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      # flash "Sign-up successful!"
+      flash[:success] = "Signup successful! Welcome to the Sample App!"
+      redirect_to @user
     else
       render 'new'
     end
