52d2a676755074b5de438f60cb32ade83a32215e
Seth NeJame <sethnejame@gmail.com>
2019-07-25 08:21:50 +0200

    Adding users as a resource, creating dynamic paths via REST

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,11 @@
 Rails.application.routes.draw do
 
-  get 'users/new'
   root "static_pages#home"
-  # get 'static_pages/help'
   get '/help', to: 'static_pages#help'
-
-  # get 'static_pages/about'
   get '/about', to: 'static_pages#about'
-
-  # get 'static_pages/contact'
   get '/contact', to: 'static_pages#contact'
-
-  # create a new path (example.com/signup) and link it to 
-  # the controller (users) and the view (users/new)
   get '/signup', to: 'users#new'
+
+  resources :users
   
 end
